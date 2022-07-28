eec63f53c5626fc7ff171be1863d8f64fdd602e2
Murilo <murilo.puche@gmail.com>
2022-07-28 11:44:34 -0300

    commit

--- src/com/inversaoDeControle/Pedido.java
+++ src/com/inversaoDeControle/Pedido.java
@@ -9,6 +9,6 @@ public class Pedido {
     }
     private EnviarEmails enviar = EnviarEmails.obterDadosEmail();
     public void gravar() {
-        this.enviar.retornar("Pedido criado!");
+        this.enviar.retornar("Pedidos criados!");
     }
 }
